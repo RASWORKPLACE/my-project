c3a72340fa7e6f4699e2f4891b9babae7ec579a2
RASWORKPLACE <74257912+RASWORKPLACE@users.noreply.github.com>
2020-11-11 05:42:32 +0530

    initial commit react native

---
+++ App-0.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Picker from './Src/components/Picker'
+const App = () => {
+  return (
+    <Picker />
+  );
+};
+export default App;
---
+++ App2.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+      <Text style={styles.instructions}>
+        Venky React Native Test Page 1
+      </Text>
+
+      <Image source={{ uri: 'https://i.imgur.com/TkIrScD.png' }} style={styles.logo} />
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
+
+      <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
+        <Text style={styles.buttonText}>Touch for Alert Message</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }, 
+});
